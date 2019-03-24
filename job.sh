d7ba077bfd998cde9dad0924c0ccf45f980eb620
tianhang zhu <tzhu71@gatech.edu>
2019-03-24 14:28:15 -0400

    job 2

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 repeat_num=1
 start_index=0
 
-time_steps=40000
+time_steps=20000
 cores_to_use=-1
 xnum=70
 ynum=70
@@ -15,10 +15,10 @@ run_and_plot_once () {
     local env=$2
 
     echo "Welcome run number $run"
-    python -m stable_baselines.ppo2.run_mujoco --env=$env --num_timesteps=$time_steps
+    python -m stable_baselines.ppo2.run_mujoco --env=$env --num-timesteps=$time_steps
 
     python -m stable_baselines.low_dim_analysis.plot_return_landscape \
-                                    --num_timesteps=$time_steps --run_num=$run --env=$env\
+                                    --num-timesteps=$time_steps --run_num=$run --env=$env\
                                     --cores_to_use=$cores_to_use --xnum=$xnum --ynum=$ynum\
                                     --padding_fraction=$padding_fraction --eval_num_timesteps=$eval_num_timesteps
 }
