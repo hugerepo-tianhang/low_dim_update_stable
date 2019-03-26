67653a49432e0d0fbb2b4045f8e948db075d33ef
tianhang zhu <tzhu71@gatech.edu>
2019-03-25 21:23:48 -0400

    run num for run

--- job.sh
+++ job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-repeat_num=4
+repeat_num=1
 start_index=0
 
-time_steps=675000
+time_steps=5000
 cores_to_use=-1
 xnum=3
 ynum=3
@@ -11,12 +11,11 @@ eval_num_timesteps=5000
 padding_fraction=0.4
 n_components=15
 
-run_and_plot_once () {
+plot_once () {
     local run=$1
     local env=$2
 
-    echo "Welcome run number $run"
-    python -m stable_baselines.ppo2.run_mujoco --env=$env --num-timesteps=$time_steps --run_num=$run
+    echo "Welcome to plot: run number $run"
 
 #    python -m stable_baselines.low_dim_analysis.plot_return_landscape \
 #                                    --num-timesteps=$time_steps --run_num=$run --env=$env\
@@ -29,13 +28,27 @@ run_and_plot_once () {
 
 }
 
+run () {
+    local run=$1
+    local env=$2
+
+    echo "Welcome to RUN: run number $run"
+    python -m stable_baselines.ppo2.run_mujoco --env=$env --num-timesteps=$time_steps --run_num=$run
+
+}
 
 for (( run_num=$start_index; run_num<$repeat_num; run_num++ ))
 do
-    sleep 5; run_and_plot_once $run_num 'Hopper-v2' ; sleep 1; ps
+    sleep 1; run $run_num 'Hopper-v2' & sleep 1; ps
+    sleep 1; run $run_num 'Walker2d-v2' & sleep 1; ps
+
 done
 
+wait
+
 for (( run_num=$start_index; run_num<$repeat_num; run_num++ ))
 do
-    sleep 5; run_and_plot_once $run_num 'Walker2d-v2' ; sleep 1; ps
+    sleep 1; plot_once $run_num 'Hopper-v2' ; sleep 1; ps
+    sleep 1; plot_once $run_num 'Walker2d-v2' ; sleep 1; ps
+
 done
